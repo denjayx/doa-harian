4cd99ae6b512d3a9ae4bfce4b77ac27126bf9c57
denjayx <denyw602@gmail.com>
2023-09-27 20:41:14 +0700

    implementasi web component

---
+++ src/components/prayer-item.js
@@ -0,0 +1,24 @@
+class PrayerItem extends HTMLElement {
+	set prayer(prayer) {
+		this._prayer = prayer;
+		this.render();
+	}
+	render() {
+		this.innerHTML = `
+      <div id="prayerItem" class="card">
+					<div class="card-header text-capitalize d-flex justify-content-between fs-6">
+						<span class="fw-bold fs-5">${this._prayer.doa}</span>
+					</div>
+					<div class="card-body">
+						<blockquote class="blockquote mb-0">
+							<p class="fs-2">${this._prayer.ayat}</p>
+							<span class="blockquote-footer"><cite title="Latin">${this._prayer.latin}</cite></span>
+							<footer class=""><cite title="Latin">${this._prayer.artinya}</cite></footer>
+						</blockquote>
+					</div>
+      </div>
+    `;
+	}
+}
+
+customElements.define("prayer-item", PrayerItem);
---
+++ src/components/prayer-list.js
@@ -0,0 +1,18 @@
+import "./prayer-item.js";
+
+class PrayerList extends HTMLElement {
+	set prayers(prayers) {
+		this._prayers = prayers;
+		this.render();
+	}
+	render() {
+		this.innerHTML = "";
+		this._prayers.forEach((prayer) => {
+			const prayerItemElement = document.createElement("prayer-item");
+			prayerItemElement.prayer = prayer;
+			this.appendChild(prayerItemElement);
+		});
+	}
+}
+
+customElements.define("prayer-list", PrayerList);
